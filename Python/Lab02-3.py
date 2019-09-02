13675f50403d68bda475ff93a5a6952f40d79af4
Tubboon <tubboon.wara@icloud.com>
2019-09-02 15:51:36 +0700

    finish Lab02-3

--- Python/Lab02-3.py
+++ Python/Lab02-3.py
@@ -13,24 +13,30 @@ from matplotlib.ticker import LinearLocator, FormatStrFormatter
 import time
 from PIL import Image
 
+
+def rescale_frame(frame, percent=75):
+    width = int(frame.shape[1] * percent/ 100)
+    height = int(frame.shape[0] * percent/ 100)
+    dim = (width, height)
+    return cv2.resize(frame, dim, interpolation =cv2.INTER_AREA)
+
 image = cv2.imread('Lab2-3.jpg',1)
 image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
 
-# plt.figure(1)
-# plt.subplot(121,title='origin')
-# plt.imshow(image)
+plt.figure(1)
+plt.subplot(121,title='origin')
+plt.imshow(image)
+# plt.subplot(122,title='origin')
+# plt.imshow(rescale_frame(image,20))
 
-# color = [[246,246,246] , [24,164,252] , [241,241,241] , [203,203,203] , [50,50,50] , [173,222,254] , [101,101,101]]
-# # n = 100
-# # color = [('o', -50, -25), ('^', -30, -5)]
-# ax = plt.figure(1).add_subplot(122, projection='3d')
-for i in image[0:1]:
-    print(i[0:])
-#     for j in i:
-#         ax.scatter(j[0], j[1], j[2], marker='o')
+ax = plt.figure(1).add_subplot(122, projection='3d')
+for i in rescale_frame(image,20):
+    for j in i:
+        print('ruinning',j[0], j[1], j[2])
+        ax.scatter(j[0], j[1], j[2], marker='o')
     
-# ax.set_xlabel('X Label')
-# ax.set_ylabel('Y Label')
-# ax.set_zlabel('Z Label')
+ax.set_xlabel('X Label')
+ax.set_ylabel('Y Label')
+ax.set_zlabel('Z Label')
 
-# plt.show()
+plt.show()
