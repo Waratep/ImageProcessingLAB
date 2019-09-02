cb54d6c1ef86e927d9dfaad95d0ea8f9d4aebfa8
Tubboon <tubboon.wara@icloud.com>
2019-09-02 09:34:32 +0700

    Lab02

--- Python/Lab02.py
+++ Python/Lab02.py
@@ -15,18 +15,39 @@ image = cv2.imread('Lab02.jpg',1)
 image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
 image_gray = cv2.cvtColor(image,cv2.COLOR_RGB2GRAY)
 
+
+Wx = np.array([[-1,-2,-1], 
+               [ 0, 0, 0],
+               [ 1, 2, 1]])   
+
+Wy = np.array([[-1, 0, 1], 
+               [-2, 0, 2],
+               [-1, 0, 1]])   
+
+Gradientx = signal.convolve2d(image_gray, Wx, boundary='symm', mode='same')
+Gradienty = signal.convolve2d(image_gray, Wy, boundary='symm', mode='same')
+
+magnitude_gradient = np.sqrt(np.power(Gradientx,2) + np.power(Gradienty,2))
+
+print('min =',np.amin(magnitude_gradient))
+print('max =',np.amax(magnitude_gradient))
+print('mean =',np.mean(magnitude_gradient))
+print('std =',np.std(magnitude_gradient))
+
+
+# calHist = cv2.calcHist([magnitude_gradient],[0],None,[256],[0,256])
+# plt.plot(histogram ,color = 'red')
+# plt.xlim([0,256])
+
 plt.figure(1)
-plt.subplot(121,title='1')
+plt.subplot(231,title='1')
 plt.imshow(image)
-plt.subplot(122,title='2')
+plt.subplot(232,title='2')
 plt.imshow(image_gray,cmap='gray')
+plt.subplot(233,title='Gradientx')
+plt.imshow(Gradientx,cmap='gray')
+plt.subplot(234,title='Gradienty')
+plt.imshow(Gradienty,cmap='gray')
+plt.subplot(235,title='magnitude_gradient')
+plt.imshow(magnitude_gradient,cmap='gray')
 plt.show()
-
-
-Wx = [[-1,-2,-1], 
-      [ 0, 0, 0],
-      [ 1, 2, 1]]   
-
-Wy = [[-1, 0, 1], 
-      [-2, 0, 2],
-      [-1, 0, 1]]   
