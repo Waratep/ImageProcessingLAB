c00614d5fdc64c403ec40db8596b8646a31f0c71
Tubboon <tubboon.wara@icloud.com>
2019-09-02 11:22:20 +0700

    finish Lab2-1

--- Python/Lab02.py
+++ Python/Lab02.py
@@ -10,6 +10,7 @@ from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm 
 from matplotlib import pyplot as plt
 from matplotlib.ticker import LinearLocator, FormatStrFormatter 
+import time
 
 image = cv2.imread('Lab02.jpg',1)
 image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
@@ -28,26 +29,54 @@ Gradientx = signal.convolve2d(image_gray, Wx, boundary='symm', mode='same')
 Gradienty = signal.convolve2d(image_gray, Wy, boundary='symm', mode='same')
 
 magnitude_gradient = np.sqrt(np.power(Gradientx,2) + np.power(Gradienty,2))
+magnitude_gradient = magnitude_gradient.astype('float32')
 
 print('min =',np.amin(magnitude_gradient))
 print('max =',np.amax(magnitude_gradient))
 print('mean =',np.mean(magnitude_gradient))
 print('std =',np.std(magnitude_gradient))
 
-
-# calHist = cv2.calcHist([magnitude_gradient],[0],None,[256],[0,256])
-# plt.plot(histogram ,color = 'red')
-# plt.xlim([0,256])
+print(magnitude_gradient.dtype)
 
 plt.figure(1)
-plt.subplot(231,title='1')
-plt.imshow(image)
-plt.subplot(232,title='2')
-plt.imshow(image_gray,cmap='gray')
-plt.subplot(233,title='Gradientx')
-plt.imshow(Gradientx,cmap='gray')
-plt.subplot(234,title='Gradienty')
-plt.imshow(Gradienty,cmap='gray')
-plt.subplot(235,title='magnitude_gradient')
-plt.imshow(magnitude_gradient,cmap='gray')
-plt.show()
+threshold = [40,50,80,120,150,160,180,200,250,300]
+count = 1
+
+# fourcc = cv2.VideoWriter_fourcc(*'DIVX')
+# out = cv2.VideoWriter('Lab02.avi',fourcc, 20.0, (magnitude_gradient.shape[1],magnitude_gradient.shape[0]),0)
+
+# for th in threshold:
+#       ret , img_threshold = cv2.threshold(magnitude_gradient,th,723,cv2.THRESH_BINARY)
+#       img_threshold = np.uint8(img_threshold)
+#       out.write(img_threshold)
+# out.release()
+# plt.show()
+
+cap = cv2.VideoCapture('Lab02.avi')
+while(cap.isOpened()):
+      ret, frame = cap.read() 
+      cv2.imshow('frame',frame)
+      if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+      time.sleep(0.5)
+ 
+cap.release()
+cv2.destroyAllWindows()
+
+# plt.figure(1)
+# plt.subplot(231,title='1')
+# plt.imshow(image)
+# plt.subplot(232,title='2')
+# plt.imshow(image_gray,cmap='gray')
+# plt.subplot(233,title='Gradientx')
+# plt.imshow(Gradientx,cmap='gray')
+# plt.subplot(234,title='Gradienty')
+# plt.imshow(Gradienty,cmap='gray')
+# plt.subplot(235,title='magnitude_gradient')
+# plt.imshow(magnitude_gradient,cmap='gray')
+
+# plt.subplot(236,title='calHist')
+# calHist = cv2.calcHist([magnitude_gradient],[0],None,[int(np.amax(magnitude_gradient))],[int(np.amin(magnitude_gradient)),int(np.amax(magnitude_gradient))])
+# plt.plot(calHist)
+# plt.xlim([0,int(np.amax(magnitude_gradient))])
+# plt.show()
